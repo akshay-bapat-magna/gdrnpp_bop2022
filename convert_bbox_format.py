1b498adfae1a56ceaed42459786653d8e6707358
Akshay Bapat <akshay.bapat1@partner.magna.com>
2023-01-11 11:42:50 -0500

    Updated script to convert GT bbox formats

--- convert_bbox_format.py
+++ convert_bbox_format.py
@@ -1,12 +1,15 @@
 import json
 from tqdm import tqdm
+from datetime import datetime
 
 
-num_scenes = 1
+num_scenes = 50
 filename = "scene_gt_info.json"
+datasetPath = "datasets/BOP_DATASETS/doorlatch/train_pbr/"
+now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
 for scene in tqdm(range(num_scenes)):
-	json_path = f"{scene:06d}/{filename}"
+	json_path = f"{datasetPath}{scene:06d}/{filename}"
 	with open(json_path, 'r') as f:
 		data = json.load(f)
 
@@ -22,3 +25,6 @@ for scene in tqdm(range(num_scenes)):
 
 	with open(json_path, 'w') as f:
 		json.dump(data, f, indent=1)
+	
+	with open(f"{datasetPath}{scene:06d}/update_log.txt", 'a') as f:
+		f.write(f"\nscene_gt.info updated on {now}")
